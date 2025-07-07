64f0990f20ed5a2daada04da9fe2b6be1f876dda
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-07-07 18:31:39 -0300

    Add:Criando a Classe Main do Game

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("RPG 2D Simples");
+    }
+}
